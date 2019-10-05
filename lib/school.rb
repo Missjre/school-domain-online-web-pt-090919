61dc7f6c2a6a1494da62f2b68178e7d92358d5aa
Jalysa Reason <jalysareason@gmail.com>
2019-10-05 00:12:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,5 +5,5 @@ end
 def roster
  School.add_student("AC Slater", 9)
  School.add_student("Kelly Kapowski", 10)
- school.add_student("Screech", 11)
- school.roster
+ School.add_student("Screech", 11)
+ School.roster
